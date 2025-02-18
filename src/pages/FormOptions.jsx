8531da2d501636aa6689c9c08417370119206add
smdimalibot <sm.dimalibot@gmail.com>
2025-02-18 14:45:04 -0500

    Removal of form-options.css and Update on FormOptions with form.css as an alternative/replacement

--- src/pages/FormOptions.jsx
+++ src/pages/FormOptions.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './css/form-options.css';
+import './css/form.css';
 
 function FormOptions() {
   const navigate = useNavigate();
 
-  const handleSignUp = () => {
-    navigate("/signup");
-  };
-
-  const handleLogIn = () => {
-    navigate("/login");
-  };
+  const handleSignUp = () => navigate("/signup");
+  const handleLogIn = () => navigate("/login");
 
   return (
-    <div className="form-options-container">
+    <div className="form-container">
       <h2>Welcome! Please choose an option:</h2>
-      <button className="form-options-button" onClick={handleSignUp}>
-        Sign Up
-      </button>
-      <button className="form-options-button" onClick={handleLogIn}>
-        Log In
-      </button>
+      <div className="button-group">
+        <button className="form-options-button" onClick={handleSignUp}>
+          Sign Up
+        </button>
+        <button className="form-options-button" onClick={handleLogIn}>
+          Log In
+        </button>
+      </div>
     </div>
   );
 }
